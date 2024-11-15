129001a69d4dd1e873032a91f2a9516c240b1fef
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-11-15 15:43:56 +0000

    initial test case running

--- SwiftXUnit/SwiftXUnit/main.swift
+++ SwiftXUnit/SwiftXUnit/main.swift
@@ -7,27 +7,58 @@
 
 import Foundation
 
-class WasRun {
-    var wasRun = false
-    
-    init(_ method: String) {
-        
+public func Assert(_ condition: @autoclosure () -> Bool, message: String = "") {
+    if condition() {
+        print("✅ Assertion passed")
+    } else {
+        print("❌ Assertion failed: \(message)")
     }
+}
+
+class TestCase: NSObject {
+    private let name: String
     
-    func testMethod() {
-        wasRun = true
+    init(_ name: String) {
+        self.name = name
     }
     
     func run() {
-        testMethod()
+        let selector = Selector(name)
+        if responds(to: selector) {
+            perform(selector)
+        } else {
+            print("Method \(name) does not exist")
+        }
     }
-    
 }
 
-let test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+class WasRun: TestCase {
+    var wasRun = false
+    
+    override init(_ name: String) {
+        super.init(name)
+    }
+    
+    @objc func testMethod() {
+        wasRun = true
+    }
+}
+
+class TestCaseTest: TestCase {
+    @objc func testRunning() {
+        let test = WasRun("testMethod")
+        Assert(test.wasRun == false)
+        test.run()
+        Assert(test.wasRun == true)
+    }
+}
+
+TestCaseTest("testRunning").run()
+
+//let test = WasRun("testMethod")
+//Assert(test.wasRun == false)
+//test.run()
+//Assert(test.wasRun == true)
 
 
 
